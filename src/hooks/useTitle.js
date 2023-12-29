4dc59a7da71f5c55f4fc07037e5d3fb522c2be9b
demo-project <nikhatmakandar30@gmail.com>
2023-12-29 08:34:57 +0530

    complete files

---
+++ src/hooks/useTitle.js
@@ -0,0 +1,10 @@
+import { useEffect } from "react";
+export const useTitle = (title) => {
+  return (
+    useEffect(()=>{
+        document.title = `${title} / MovieMate`;
+      },[])
+    
+  )
+}
+
